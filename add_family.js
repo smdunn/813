26cf0baf635d92d603f8de02e58e96ee50363528
Rachel Rotteveel <rachelrotteveel@dhcp-18-111-76-143.dyn.mit.edu>
2016-04-10 16:29:10 -0400

    Can add a family

--- add_family.js
+++ add_family.js
@@ -4,5 +4,8 @@ $(function() {
     });
     $("#btnOK").click(function() {
         $("#dialogAddFamily").hide();
+        var child = $('#childs_name').val();
+        $('#sidebar_order').append('<tr><td><input type="button" id="events_button" value="' + child + '\'s Family"></input></td></tr>');
+        $('#childs_name').val('');
     });
 });
