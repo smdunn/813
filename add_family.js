a155091b75a35835216e1922452204fd16771746
Rachel Rotteveel <rachelrotteveel@dhcp-18-111-76-143.dyn.mit.edu>
2016-04-10 16:41:13 -0400

    Can't add empty family name

--- add_family.js
+++ add_family.js
@@ -3,9 +3,11 @@ $(function() {
         $("#dialogAddFamily").show();
     });
     $("#btnOK").click(function() {
-        $("#dialogAddFamily").hide();
         var child = $('#childs_name').val();
-        $('#sidebar_order').append('<tr><td><input type="button" id="events_button" value="' + child + '\'s Family"></input></td></tr>');
-        $('#childs_name').val('');
+        if (child.length > 0) {
+        	$("#dialogAddFamily").hide();
+        	$('#sidebar_order').append('<tr><td><input type="button" id="events_button" value="' + child + '\'s Family"></input></td></tr>');
+        	$('#childs_name').val('');
+        }
     });
 });
