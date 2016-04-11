ca39ae4b9aa0d65b607c3f15ce67838f3faea1b5
Katie <kpvm@mit.edu>
2016-04-11 15:16:13 -0400

    added cancel button to popups

--- add_event.js
+++ add_event.js
@@ -4,11 +4,24 @@ $(function() {
     });
     $("#addEventOk").click(function() {
         $("#dialogAddEvent").hide();
-        var month = $('#monthInput').val();
-        var day = $('#dayInput').val();
+        var month = $('#monthInputEvent').val();
+        var day = $('#dayInputEvent').val();
         var person = $('#person').val();
         var event = $('#event').val();
         $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
            + '\'s ' + event + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
+        $('#person').val('');
+        $('#monthInputEvent').val('');
+        $('#dayInputEvent').val('');
+        $('#yearInputEvent').val('');
+        $('#event').val('');
+    });
+    $('#addEventCancel').click(function() {
+        $("#dialogAddEvent").hide();
+        $('#person').val('');
+        $('#monthInputEvent').val('');
+        $('#dayInputEvent').val('');
+        $('#yearInputEvent').val('');
+        $('#event').val('');
     });
 });
--- add_family.js
+++ add_family.js
@@ -8,6 +8,18 @@ $(function() {
         	$("#dialogAddFamily").hide();
         	$('#sidebar_order').append('<tr><td><input type="button" id="events_button" value="' + child + '\'s Family"></input></td></tr>');
         	$('#childs_name').val('');
+            $('#monthInputFam').val('');
+            $('#dayInputFam').val('');
+            $('#yearInputFam').val('');
+            $('#childs_email').val('');
         }
+    });
+    $('#btnCancel').click(function() {
+        $("#dialogAddFamily").hide();
+        $('#childs_name').val('');
+        $('#monthInputFam').val('');
+        $('#dayInputFam').val('');
+        $('#yearInputFam').val('');
+        $('#childs_email').val('');
     });
 });
