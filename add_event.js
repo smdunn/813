0c8d06aa32a157d5feaf4ed49c4127d677d324ff
Shannon Dunn <smdunn@dhcp-18-111-13-244.dyn.MIT.EDU>
2016-04-11 14:10:16 -0400

    updated

--- add_event.js
+++ add_event.js
@@ -7,8 +7,8 @@ $(function() {
         var month = $('#monthInput').val();
         var day = $('#dayInput').val();
         var person = $('#person').val();
-        var event = $('#event').val();
+        var e = $('#event').val();
         $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
-           + '\'s ' + event + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
+           + '\'s ' + e + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
     });
 });
