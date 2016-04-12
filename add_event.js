8ee78d32ca9dda3c0702a17b73328dc1ddbd0bbb
Nicole O'Brien <njobrien@mit.edu>
2016-04-12 17:25:48 -0400

    changing variable names to not overlap and trying to get the add event thing to work

--- add_event.js
+++ add_event.js
@@ -26,9 +26,30 @@
 //     });
 // });
 
-$('#btnAddEvent').popover({
+
+
+$(function(){
+	$('#btnAddEvent').popover({
     html: true,
     title: 'Add Event<a class="close" href="#");">&times;</a>',
     placement: 'bottom',
     content: $('#popoverAddEvent').html(),
-});
+	});
+
+	$("#btnOkEvent").click(function(){
+		console.log("ok")
+		$("#btnAddEvent").popover('hide');
+        var month = $('#inputMonthEvent').val();
+        var day = $('#inputDayEvent').val();
+        var person = $('#inputPerson').val();
+        var e = $('#event').val();
+        $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
+           + '\'s ' + e + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
+        $('#person').val('');
+        $('#monthInputEvent').val('');
+        $('#dayInputEvent').val('');
+        $('#yearInputEvent').val('');
+        $('#event').val('');
+
+	})
+})
