3abfdae9479b76193bd923f1403bdd9cfaa3d1de
Katie <kpvm@mit.edu>
2016-04-12 10:09:34 -0400

    added event and family popover inputs

--- add_event.js
+++ add_event.js
@@ -1,27 +1,34 @@
-$(function() {
-    $("#add_event").click(function() {
-        $("#dialogAddEvent").show();
-    });
-    $("#addEventOk").click(function() {
-        $("#dialogAddEvent").hide();
-        var month = $('#monthInputEvent').val();
-        var day = $('#dayInputEvent').val();
-        var person = $('#person').val();
-        var e = $('#event').val();
-        $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
-           + '\'s ' + e + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
-        $('#person').val('');
-        $('#monthInputEvent').val('');
-        $('#dayInputEvent').val('');
-        $('#yearInputEvent').val('');
-        $('#event').val('');
-    });
-    $('#addEventCancel').click(function() {
-        $("#dialogAddEvent").hide();
-        $('#person').val('');
-        $('#monthInputEvent').val('');
-        $('#dayInputEvent').val('');
-        $('#yearInputEvent').val('');
-        $('#event').val('');
-    });
+// $(function() {
+//     $("#add_event").click(function() {
+//         $("#dialogAddEvent").show();
+//     });
+//     $("#addEventOk").click(function() {
+//         $("#dialogAddEvent").hide();
+//         var month = $('#monthInputEvent').val();
+//         var day = $('#dayInputEvent').val();
+//         var person = $('#person').val();
+//         var e = $('#event').val();
+//         $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
+//            + '\'s ' + e + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
+//         $('#person').val('');
+//         $('#monthInputEvent').val('');
+//         $('#dayInputEvent').val('');
+//         $('#yearInputEvent').val('');
+//         $('#event').val('');
+//     });
+//     $('#addEventCancel').click(function() {
+//         $("#dialogAddEvent").hide();
+//         $('#person').val('');
+//         $('#monthInputEvent').val('');
+//         $('#dayInputEvent').val('');
+//         $('#yearInputEvent').val('');
+//         $('#event').val('');
+//     });
+// });
+
+$('#btnAddEvent').popover({
+    html: true,
+    title: 'Add Event<a class="close" href="#");">&times;</a>',
+    placement: 'bottom',
+    content: $('#popoverAddEvent').html(),
 });
--- add_family.js
+++ add_family.js
@@ -1,25 +1,31 @@
-$(function() {
-    $("#btnAddFamily").click(function() {
-        $("#dialogAddFamily").show();
-    });
-    $("#btnOK").click(function() {
-        var child = $('#childs_name').val();
-        if (child.length > 0) {
-        	$("#dialogAddFamily").hide();
-        	$('#sidebar_order').append('<tr><td><input type="button" id="events_button" value="' + child + '\'s Family"></input></td></tr>');
-        	$('#childs_name').val('');
-            $('#monthInputFam').val('');
-            $('#dayInputFam').val('');
-            $('#yearInputFam').val('');
-            $('#childs_email').val('');
-        }
-    });
-    $('#btnCancel').click(function() {
-        $("#dialogAddFamily").hide();
-        $('#childs_name').val('');
-        $('#monthInputFam').val('');
-        $('#dayInputFam').val('');
-        $('#yearInputFam').val('');
-        $('#childs_email').val('');
-    });
+// $(function() {
+//     $("#btnAddFamily").click(function() {
+//         $("#dialogAddFamily").show();
+//     });
+//     $("#btnOK").click(function() {
+//         var child = $('#childs_name').val();
+//         if (child.length > 0) {
+//         	$("#dialogAddFamily").hide();
+//         	$('#sidebar_order').append('<tr><td><input type="button" id="events_button" value="' + child + '\'s Family"></input></td></tr>');
+//         	$('#childs_name').val('');
+//             $('#monthInputFam').val('');
+//             $('#dayInputFam').val('');
+//             $('#yearInputFam').val('');
+//             $('#childs_email').val('');
+//         }
+//     });
+//     $('#btnCancel').click(function() {
+//         $("#dialogAddFamily").hide();
+//         $('#childs_name').val('');
+//         $('#monthInputFam').val('');
+//         $('#dayInputFam').val('');
+//         $('#yearInputFam').val('');
+//         $('#childs_email').val('');
+//     });
+// });
+
+$('#btnAddFamily').popover({
+    html: true,
+    title: 'Popover Title<a class="close" href="#");"></a>',
+    content: $('#popoverAddFamily').html(),
 });
