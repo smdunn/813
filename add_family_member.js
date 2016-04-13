09ef9fcfb4afa6c739ed0e213169b2548ff83988
Shannon Dunn <smdunn@Shannons-MacBook-Pro.local>
2016-04-13 18:05:11 -0400

    fixed stuff

--- add_family_member.js
+++ add_family_member.js
@@ -16,10 +16,10 @@ $(function(){
         var relationship = $('input:radio[name=optradio]').filter(":checked").val()
         console.log(relationship);
         if (relationship=="spouse"){
-            $("#"+child+"div").append("<li><a>"+grandchild+"</a></li>");
+            $("#"+child+"div").append("<li><a>"+grandchild+"<br>1. Peacoat<br>2. Ovenmitt<br>3. Red Sox Tickets</a></li>");
         }
         else{
-        $('#'+child+"kids").append("<li><a>"+grandchild+"</a></li>");
+            $('#'+child+"kids").append("<li><a>"+grandchild+"<br>1. Sneakers<br>2. Beats<br>3. Frisbee</a></li>");
     }
     });
 });
