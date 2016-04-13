8539cce81894fb8afeaa6cf586461bd1fe183bb3
Shannon Dunn <smdunn@dhcp-18-111-87-38.dyn.MIT.EDU>
2016-04-13 01:25:45 -0400

    relationship added

--- add_family_member.js
+++ add_family_member.js
@@ -14,7 +14,11 @@ $(function(){
         var day= $("#dayfammember").val();
         var year= $("#yearfammember").val();
         var relationship = $("#relationship").val();
+        if (relationship=="spouse"){
+            $("#"+child+"div").append("<li><a>"+grandchild+"</a><ul><div id='"+grandchild+"kids'></div></ul></li>");
+        }
+        else{
         $('#'+child+"kids").append("<li><a>"+grandchild+"</a></li>");
-
+    }
     });
 });
