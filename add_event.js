030447e13a46fa45ae46641571f7766408d827d2
Rachel Rotteveel <rachelrotteveel@dhcp-18-111-113-25.dyn.MIT.EDU>
2016-04-12 20:14:49 -0400

    Fixed add event

--- add_event.js
+++ add_event.js
@@ -27,29 +27,31 @@
 // });
 
 
-
 $(function(){
 	$('#btnAddEvent').popover({
-    html: true,
-    title: 'Add Event<a class="close" href="#");">&times;</a>',
-    placement: 'bottom',
-    content: $('#popoverAddEvent').html(),
+        html: true,
+        title: 'Add Event<a class="close" href="#");">&times;</a>',
+        placement: 'bottom',
+        content: $('#popoverAddEvent').html(),
 	});
 
-	$("#btnOkEvent").click(function(){
-		console.log("ok")
+	$(document).on('click', '#btnOkEvent', function(){
+		console.log("ok");
 		$("#btnAddEvent").popover('hide');
         var month = $('#inputMonthEvent').val();
         var day = $('#inputDayEvent').val();
         var person = $('#inputPerson').val();
-        var e = $('#event').val();
-        $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
-           + '\'s ' + e + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
+        var e = $('#inputEvent').val();
+        console.log(e);
+        // $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
+        //    + '\'s ' + e + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
+        $('#events_table').append('<tr><td>' + month + day + '</td><td>' + person
+           + '\'s ' + e + '</td><td><button type="button" class="btn btn-default">View</button></td></tr>');
         $('#person').val('');
         $('#monthInputEvent').val('');
         $('#dayInputEvent').val('');
         $('#yearInputEvent').val('');
-        $('#event').val('');
+        $('#inputEvent').val('');
 
-	})
-})
+	});
+});
