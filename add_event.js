f2f28215582934c2ce744d47a120ccca5eaa55c5
Nicole O'Brien <njobrien@mit.edu>
2016-04-30 19:40:25 -0400

    commit

--- add_event.js
+++ add_event.js
@@ -1,3 +1,30 @@
+// $(function() {
+//     $("#add_event").click(function() {
+//         $("#dialogAddEvent").show();
+//     });
+//     $("#addEventOk").click(function() {
+//         $("#dialogAddEvent").hide();
+//         var month = $('#monthInputEvent').val();
+//         var day = $('#dayInputEvent').val();
+//         var person = $('#person').val();
+//         var e = $('#event').val();
+//         $('#events_table tr.add_event_row').before('<tr class="border"><td class="date">' + month + day + '</td><td class="event">' + person
+//            + '\'s ' + e + '</td><td class="wishlist"><input type="button" id="view_wishlist" value="View"></input></td>)</tr>');
+//         $('#person').val('');
+//         $('#monthInputEvent').val('');
+//         $('#dayInputEvent').val('');
+//         $('#yearInputEvent').val('');
+//         $('#event').val('');
+//     });
+//     $('#addEventCancel').click(function() {
+//         $("#dialogAddEvent").hide();
+//         $('#person').val('');
+//         $('#monthInputEvent').val('');
+//         $('#dayInputEvent').val('');
+//         $('#yearInputEvent').val('');
+//         $('#event').val('');
+//     });
+// });
 wishlists=[];
 wishlists.push("Patagonia Fleece");
 wishlists.push("Rainboots");
@@ -44,23 +71,12 @@ wishlists.push("Notebooks");
 wishlists.push("Caligraphy Pens");
 
 
-var tmp = $.fn.popover.Constructor.prototype.show;
-$.fn.popover.Constructor.prototype.show = function () {
-  tmp.call(this);
-  if (this.options.callback) {
-    this.options.callback();
-  }
-}
-
 $(function(){
 	$('#btnAddEvent').popover({
         html: true,
         title: 'Add Event<a class="close" href="#");">&times;</a>',
         placement: 'bottom',
         content: $('#popoverAddEvent').html(),
-        callback: function() { 
-            $('.datepicker').datepicker(); 
-        } 
 	});
 
     $(document).on("click", ".close" , function(){
@@ -85,8 +101,8 @@ $(function(){
                         here=true;
                     }
                 }
-                $('#events_table').append('<tr><td>' + month + ' ' + day + '</td><td><input type="button" class="link" id="' +person+'link" value=" '+person+'">'
-            + '\'s </input>' + e + '</td><td id="'+person+'col"><input type="button" class="btn btn-default" id="btn'+person+'view"  value="View"></input><div class="collapse out" id="'+person+'list"><ul><li>'+persondict.wishlist[0]+'</li><li>'+persondict.wishlist[1]+' </li><li> '+persondict.wishlist[2]+'</li></ul></div></td></tr>');
+                $('#events_table').append('<tr><td>' + month + ' ' + day + '</td><td><input type="button" class="link" id="' +person+'link" value=" '+person+'\'s ">'
+            + '</input>' + e + '</td><td id="'+person+'col"><input type="button" class="btn btn-default" id="btn'+person+'view"  value="View"></input><div class="collapse out" id="'+person+'list"><ul><li>'+persondict.wishlist[0]+'</li><li>'+persondict.wishlist[1]+' </li><li> '+persondict.wishlist[2]+'</li></ul></div></td></tr>');
             $('#person').val('');
             $('#events_table').tablesorter({ 
                            headers: {
@@ -165,8 +181,8 @@ $(function(){
                     }
                 }
             if (here){
-            $('#events_table').append('<tr><td id="date">' + getMonth(month) + ' ' + day + '</td><td><input type="button" class="link" id="'+person+'link" value=" '+person+'">'
-           + '\'s </input>' + 'Birthday' + '</td><td id="'+person+'col"><input type="button" class="btn btn-default" id="btn'+person+'view"  value="View"></input><div class="collapse out" id="'+person+'list"><ul><li>'+persondict.wishlist[0]+'</li><li> '+persondict.wishlist[1]+'</li><li> '+persondict.wishlist[2]+'</li></ul></div></td></tr>');
+            $('#events_table').append('<tr><td id="date">' + getMonth(month) + ' ' + day + '</td><td><input type="button" class="link" id="'+person+'link" value=" '+person+'\'s ">'
+           + '</input>' + 'Birthday' + '</td><td id="'+person+'col"><input type="button" class="btn btn-default" id="btn'+person+'view"  value="View"></input><div class="collapse out" id="'+person+'list"><ul><li>'+persondict.wishlist[0]+'</li><li> '+persondict.wishlist[1]+'</li><li> '+persondict.wishlist[2]+'</li></ul></div></td></tr>');
             }
             else{
 
--- add_family_member.js
+++ add_family_member.js
@@ -126,8 +126,8 @@ $(function(){
                 $('#'+child+"kids").append("<li><a><h3>"+grandchild+"</h3>1."+grandchilddict.wishlist[0]+"<br>2."+grandchilddict.wishlist[1]+"<br>3. "+grandchilddict.wishlist[2]+"</a></li>");
             }
 
-            $('#events_table').append('<tr><td>' + getMonth(month) + ' ' + day + '</td><td><input type="button" class="link" id="'+grandchild+'link" value=" '+grandchild+'">'
-               + '\'s </input>' + 'Birthday' + '</td><td id="'+grandchild+'col"><input type="button" class="btn btn-default" id="btn'+grandchild+'view"  value="View"></input></td></tr><tr class="collapse out" id="'+grandchild+'list"><ul><li>1.'+grandchilddict.wishlist[0]+'<br>2.'+grandchilddict.wishlist[1]+'<br>3. '+grandchilddict.wishlist[2]+'</li></ul></td></tr>');
+            $('#events_table').append('<tr><td>' + getMonth(month) + ' ' + day + '</td><td><input type="button" class="link" id="'+grandchild+'link" value=" '+grandchild+'\'s ">'
+               + '</input>' + 'Birthday' + '</td><td id="'+grandchild+'col"><input type="button" class="btn btn-default" id="btn'+grandchild+'view"  value="View"></input></td></tr><tr class="collapse out" id="'+grandchild+'list"><td><ul><li>1.'+grandchilddict.wishlist[0]+'<br>2.'+grandchilddict.wishlist[1]+'<br>3. '+grandchilddict.wishlist[2]+'</li></ul></tr>');
             $("#btnAddFamilyMember").popover('hide');
         }
 
